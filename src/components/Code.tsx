a51754c0284d0d096245d98f4ca2eb27c0fcc6ac
Lu <66273292+thisdotLolu@users.noreply.github.com>
2024-07-10 11:09:04 +0100

    next auth ig

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -1,8 +1,10 @@
 'use client'
 
 import React, { FC, useEffect, useState } from 'react'
-import {type Language} from 'prism-react-renderer'
+import {Highlight,  type Language,} from 'prism-react-renderer'
 import { useTheme } from 'next-themes'
+// import darkTheme from 'prism-react-renderer/themes/nightOwl'
+
 
 interface CodeProps {
     code:string
@@ -34,8 +36,38 @@ const Code:FC <CodeProps> = ({
             }, animationDelay || 150)
         }
     },[code, show, animated,animationDelay])
+    const lines = text.split(/\r\n|\r|\n/).length
+
+    // const theme = applicationTheme === 'light' ? lightTheme : darkTheme
+
   return (
-    <div>Code</div>
+    <Highlight code={text} language={language}>
+        {({className, tokens, getLineProps, getTokenProps})=> <pre
+        className={
+            className + 
+            'transition-all w-fit bg-transparent duration-100 py-0 no-scrollbar'
+        }
+        style={{
+            maxHeight:show?lines * 24 : 0,
+            opacity:show? 1:0
+        }}
+        >
+            {tokens.map((line, i)=>{
+                const {key, ...rest} = getLineProps({line,key:i})
+                return (
+                    <div key={`line-${i}`}
+                    style={{position:'relative'}}
+                    {...rest}
+                    >
+                        {line.map((token,index)=>{
+                            const {key, ...props} = getTokenProps({token, i})
+                            return <span key={index}{...props}></span>
+                        })}
+                    </div>
+                )
+            })}
+        </pre>}
+    </Highlight>
   )
 }
 
